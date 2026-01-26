2b3eb71eb7b77b70761e598b493bc94957636192
Isaac Wight <isaac.c.wight@gmail.com>
2026-01-26 12:09:54 -0800

    grass, pokemon, and pinky pinching spawn added

--- openCV/main.py
+++ openCV/main.py
@@ -2,12 +2,17 @@ import cv2
 from ultralytics import YOLO
 import mediapipe as mp
 import math
+import time
 
 model = YOLO("yolov8n.pt")
 cap = cv2.VideoCapture(0)
 
 screenHeight = 720
 screenWidth = 1280
+
+pokemon = None
+
+pokeballopened = False
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, screenWidth)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, screenHeight)
@@ -18,6 +23,23 @@ mp_draw = mp.solutions.drawing_utils
 sprite = cv2.imread("pokeball.png", cv2.IMREAD_UNCHANGED)
 sprite = cv2.resize(sprite, (100, 100))
 sh, sw = sprite.shape[:2]
+
+grass = cv2.imread("grass.png", cv2.IMREAD_UNCHANGED)
+grass = cv2.resize(grass, (300, 300))
+gh, gw = grass.shape[:2]
+gx1 = screenWidth - gw
+gx2 = gx1 + gw
+gy1 = screenHeight - gh
+gy2 = gy1 + gh
+
+pokemon = cv2.imread("squirtle.png", cv2.IMREAD_UNCHANGED)
+pokemon = cv2.resize(pokemon, (150, 150))
+ph, pw = pokemon.shape[:2]
+px1 = 500
+px2 = px1 + pw
+py1 = 300
+py2 = py1 + ph
+pokemon[:, :, 3] = 0
 
 wasGrabbed = False
 x1, y1 = 100, 100
@@ -73,13 +95,22 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) a
 
                 thumb = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
                 index = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                pinky = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP]
 
                 dx = thumb.x - index.x
                 dy = thumb.y - index.y
                 dist = math.sqrt(dx*dx + dy*dy)
 
+                dpx = thumb.x - pinky.x
+                dpy = thumb.y - pinky.y
+                distp = math.sqrt(dpx*dpx + dpy*dpy)
+
+
                 pinch_x = int((thumb.x + index.x) * 0.5 * frame.shape[1])
                 pinch_y = int((thumb.y + index.y) * 0.5 * frame.shape[0])
+
+                pinchp_x = int((thumb.x + pinky.x) * 0.5 * frame.shape[1])
+                pinchp_y = int((thumb.y + pinky.y) * 0.5 * frame.shape[0])
 
                 if grabbing:
                     if prev_pinch is not None:
@@ -98,6 +129,18 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) a
                         y2 = y1 + sh
                     vx = int(vx * damping)
                     vy = int(vy * damping)
+                    if x1 >= px1 - 25 and x2 <= px1 + pw + 25 and y1 >= py1 - 25 and y2 <= py1 + ph + 25:
+                        sprite = cv2.imread("open_pokeball.png", cv2.IMREAD_UNCHANGED)
+                        sprite = cv2.resize(sprite, (100, 100))
+                        pokeballopened = True
+                        current_time = time.time()
+                        vx = 0
+                        vy = 0
+
+                if pokeballopened and time.time() - current_time > 1.0:
+                    sprite = cv2.imread("pokeball.png", cv2.IMREAD_UNCHANGED)
+                    sprite = cv2.resize(sprite, (100, 100))
+                    pokeballopened = False
 
                 if dist < 0.05:   # pinching
                     grabbing = True
@@ -111,6 +154,13 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) a
                             y1 = pinch_y - sh // 2
                             x2 = x1 + sw
                             y2 = y1 + sh
+
+
+
+        frame = draw_sprite(frame, grass, gx1, gy1)
+        if distp < 0.05 and pinchp_x > px1 and pinchp_x < px2 and pinchp_y > py1 and pinchp_y < py2:
+            pokemon[:, :, 3] = 255
+            frame = draw_sprite(frame, pokemon, px1, py1)
         frame = draw_sprite(frame, sprite, x1, y1)
         cv2.imshow("Pose Tracking", frame)
 
